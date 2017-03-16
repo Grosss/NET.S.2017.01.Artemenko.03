015bd585f0025dfd238536168ff09889d9866af8
Grosss <artemenko,nickolai@gmail.com>
2017-03-16 15:03:06 +0200

    TODO: fix troubles with negative numbers

--- Task3.CUI/Program.cs
+++ Task3.CUI/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Task3;
 
 namespace Task3.CUI
@@ -11,7 +7,13 @@ namespace Task3.CUI
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine(BitInsertion.PassNumbersAndBits(8, 15, 0, 0));
+            Console.WriteLine(BitInsertion.PassNumbersAndBits(0, 15, 30, 30));
+            Console.WriteLine(BitInsertion.PassNumbersAndBits(0, 15, 0, 30));
+            Console.WriteLine(BitInsertion.PassNumbersAndBits(15, -15, 0, 4));
+            Console.WriteLine(BitInsertion.PassNumbersAndBits(15, int.MaxValue, 3, 5));
+
+            Console.ReadLine();
         }
     }
 }
--- Task3/BitInsertion.cs
+++ Task3/BitInsertion.cs
@@ -10,7 +10,50 @@ namespace Task3
     {
         public static int PassNumbersAndBits(int firstNumb, int secondNumb, int firstBitPos, int secondBitPos)
         {
-            return 0;
+            if (firstBitPos > secondBitPos)
+                throw new ArgumentException();
+            byte[] firstBinaryNumb = TransformIntoBitArray(firstNumb);
+            byte[] secondBinaryNumb = TransformIntoBitArray(secondNumb);
+            InsertBits(firstBinaryNumb, secondBinaryNumb, firstBitPos, secondBitPos);
+            
+            return TransformIntoDecimal(firstBinaryNumb);
         }
+
+        #region private methods
+        private static byte[] TransformIntoBitArray(int numberToTransform)
+        {
+            byte[] binaryNumb = new byte[32];
+            if (numberToTransform < 0)
+            {
+                binaryNumb[31] = 1;
+                numberToTransform -= int.MinValue;
+            }
+            for(int i = 0; i < 31; i++)
+            {
+                binaryNumb[i] = (byte) (numberToTransform % 2);
+                numberToTransform /= 2;
+            }
+            return binaryNumb;
+        }
+
+        private static void InsertBits(byte[] firstNumb, byte[] secondNumb, int firstPos, int secondPos)
+        {
+            int secondNumbIndex = 0;
+            for(int i = firstPos; i < secondPos + 1; i++)
+            {
+                firstNumb[i] = secondNumb[secondNumbIndex++];
+            }
+        }
+
+        private static int TransformIntoDecimal(byte[] binaryNumb)
+        {
+            int result = binaryNumb[0];
+            for (int i = 1; i < 31; i++)
+                result += (int) Math.Pow(binaryNumb[i] * 2, i);
+            if (binaryNumb[31] == 1)
+                result += int.MinValue;
+            return result;
+        }
+        #endregion
     }
 }
